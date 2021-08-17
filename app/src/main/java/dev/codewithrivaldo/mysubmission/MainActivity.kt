0070a22a8fe189acc0efabdca73e7dbe43589021
unknown <vlado11jr@gmail.com>
2021-08-17 21:48:38 +0700

    [Rivaldo] Initial commit

---
+++ app/src/main/java/dev/codewithrivaldo/mysubmission/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.codewithrivaldo.mysubmission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
