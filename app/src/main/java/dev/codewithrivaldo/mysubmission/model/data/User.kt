0645d9287074727fc86510abd08f7073517563c3
unknown <vlado11jr@gmail.com>
2021-08-18 20:26:11 +0700

    [Rivaldo] Create data class with parcelable

---
+++ app/src/main/java/dev/codewithrivaldo/mysubmission/model/data/User.kt
@@ -0,0 +1,14 @@
+package dev.codewithrivaldo.mysubmission.model.data
+
+import android.os.Parcelable
+
+data class User(
+    val name: String,
+    val username: String,
+    val avatar: Int,
+    val company: String,
+    val location: String,
+    val repository: Int,
+    val follower: Int,
+    val following: Int
+): Parcelable //add parcelable
